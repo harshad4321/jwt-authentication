38299612d96f7b4a04192efd61171c9f63dc7d22
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-08-19 15:27:30 +0530

    accessToken

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 const jwt = require('jsonwebtoken')
 app.use (express.json())
+
 
 
 const posts = [
@@ -26,8 +29,8 @@ app.post('/login',(req,res)=>{
     const username = req.body.username
 const user ={ name :username}
 
-
-   const accessToken=  jwt.sign(user, process.env.ACCESSS_TOKEN_SECRET)
+   const accessToken =  jwt.sign(user,
+     process.env.ACCESSS_TOKEN_SECRET)
      res.json ({accessToken:accessToken})
 })
 
