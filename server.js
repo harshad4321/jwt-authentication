3ae0e1bc7c4c4d9f655989d17619b8e462aedb67
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-08-19 17:41:17 +0530

    accessToken set

--- server.js
+++ server.js
@@ -19,10 +19,12 @@ const posts = [
    }
 ]
 
-app.post ('/post ',(req, res)=>{
-res.json(posts)
+app.get ('/posts',authenticateToken,(req, res)=>{
+ 
+res.json(posts.filter(post => post.username === req.user.name))
 
 }) 
+
 app.post('/login',(req,res)=>{
     //authentication
 
@@ -31,8 +33,19 @@ const user ={ name :username}
 
    const accessToken =  jwt.sign(user,
      process.env.ACCESSS_TOKEN_SECRET)
-     res.json ({accessToken:accessToken})
+     res.json ({accessToken: accessToken})
 })
+function authenticateToken(req,res,next){
+     const authHeader = req.headers['authorization']
+     const  token  = authHeader && authHeader.split(' ')[1]
+ if (token == null) return  res.sendStatus(401)
 
+ jwt.verify(token,process.env.ACCESSS_TOKEN_SECRET,(err,user)=>{
+    if (err) return res.sendStatus(403) 
+    req.user = user
+    next()
+ })
+
+}
 
 app.listen(3000)
